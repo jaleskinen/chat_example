08f942a9bdaf1620adb5763b3ee02c809a78798c
jaleskinen <jarmo.leskinen@hepokumpu.com>
2016-01-22 12:46:29 +0200

    22-01-2016 Jarmo Leskinen, more functionality added

--- index.js
+++ index.js
@@ -16,12 +16,32 @@ io.on('connection', function (socket) {
 
 //Make root context router to return index.html
 express.get('/', function (req, res) {
-    res.sendfile('index.html');
+    res.sendfile('./view/index.html');
 });
 
 //Make chat_scripts.js router to return chat_scripts.js
 express.get('/chat_scripts.js', function (req, res) {
-    res.sendfile('chat_scripts.js');
+    res.sendfile('./lib/chat_scripts.js');
+});
+
+//Make jquery-2.1.4.min.js router to return jquery-2.1.4.min.js
+express.get('/jquery-2.1.4.min.js', function (req, res) {
+    res.sendfile('./lib/jquery-2.1.4.min.js');
+});
+
+//Make jquery-2.1.4.min.js router to return jquery-2.1.4.min.js
+express.get('/bootstrap.min.js', function (req, res) {
+    res.sendfile('./lib/bootstrap.min.js');
+});
+
+//Make styles.css router to return styles.css
+express.get('/styles.css', function (req, res) {
+    res.sendfile('./css/styles.css');
+});
+
+//Make styles.css router to return styles.css
+express.get('/bootstrap.min.css', function (req, res) {
+    res.sendfile('./css/bootstrap.min.css');
 });
 
 http.listen(3000, function () {
